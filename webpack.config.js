658da555ce168341d414701ed54d618f9b0835ca
Robert Blutner <robert@blutner.de>
2018-01-06 00:11:27 +0100

    Router

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'www'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/user/'
     },
     module: {
         loaders: [
